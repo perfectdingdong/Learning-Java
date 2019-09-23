27a6969e96e7bea9c0355291f33c622c750b423c
dzh <perfectdingdong@gmail.com>
2019-09-24 01:31:42 +0800

    实现了Java的动态编译技术

---
+++ src/main/java/com/study/java/config/swaggerConfig.java
@@ -0,0 +1,41 @@
+package com.study.java.config;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class swaggerConfig {
+
+    @Bean
+    public Docket petApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.study.java.controller")) //指定提供接口所在的基包
+                .build();
+    }
+
+    /**
+     * 该套 API 说明，包含作者、简介、版本、host、服务URL
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("demo api 说明")
+                .contact(new Contact("allen","null","name@example.com"))
+                .version("0.1")
+                .termsOfServiceUrl("localhost:8080/demo1/")
+                .description("demo api")
+                .build();
+    }
+}
--- src/main/java/com/study/java/demo/DemoApplication.java
+++ src/main/java/com/study/java/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.study.java.demo;
 
+import com.study.java.demo.Just_in_time_compilation.demo1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,31 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+        String code = "public class HelloWorld {\n" +
+                "    public static void main(String []args) {\n" +
+                "\t\tfor(int i=0; i < 2; i++){\n" +
+                "\t\t\t       System.out.println(\"Hello World!\");\n" +
+                "\t\t}\n" +
+                "    }\n" +
+                "}";
+        demo1 compiler = new demo1(code);
+        boolean res = compiler.compiler();
+        if (res) {
+            System.out.println("编译成功");
+            System.out.println("compilerTakeTime：" + compiler.getCompilerTakeTime());
+            try {
+                compiler.runMainMethod();
+                System.out.println("runTakeTime：" + compiler.getRunTakeTime());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println(compiler.getRunResult());
+            System.out.println("诊断信息：" + compiler.getCompilerMessage());
+        } else {
+            System.out.println("编译失败");
+            System.out.println(compiler.getCompilerMessage());
+        }
     }
 
 }
